0330827ec52545a3b7b6d0d96a40d3ea71404f3e
yatharth1706 <yatharthverma070@gmail.com>
2023-07-16 13:21:20 +0530

    Add open ai apis. Pending parsing so that form can be made

--- src/app/(protected)/app/useai/page.js
+++ src/app/(protected)/app/useai/page.js
@@ -1,10 +1,25 @@
 "use client";
-import { PlusIcon } from "lucide-react";
+import { Configuration, OpenAIApi } from "openai-edge";
+import { OpenAIStream, StreamingTextResponse } from "ai";
 import React, { useEffect, useState } from "react";
 import { toast } from "react-toastify";
+import { useRouter } from "next/navigation";
+import { useFormVibeContext } from "@/src/contexts/FormVibeContextProvider";
 
 function UseAIPage() {
   const [openAIKey, setOpenAIKey] = useState();
+  const [prompt, setPrompt] = useState("");
+  const [isGeneratingForm, setIsGeneratingForm] = useState(false);
+  const { createForm } = useFormVibeContext();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (typeof window !== undefined) {
+      let openaikey = window.localStorage.getItem("OpenAIKeyFormVibe");
+      setOpenAIKey(openaikey ?? "");
+    }
+  }, []);
+
   const handleSaveOpenAIKey = () => {
     window.localStorage.setItem("OpenAIKeyFormVibe", openAIKey);
     toast("Saved to local storage successfully", {
@@ -14,12 +29,155 @@ function UseAIPage() {
     });
   };
 
-  useEffect(() => {
-    if (typeof window !== undefined) {
-      let openaikey = window.localStorage.getItem("OpenAIKeyFormVibe");
-      setOpenAIKey(openaikey ?? "");
+  const handlePrompts = async () => {
+    try {
+      setIsGeneratingForm(true);
+      const configuration = new Configuration({
+        apiKey: openAIKey,
+      });
+
+      const openai = new OpenAIApi(configuration);
+
+      const chat_completion = await openai.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages: [
+          {
+            role: "user",
+            content:
+              `i have this array with various form elements and thier label names and other properties of element. const jobAppFormEls = [
+          {
+            id: 1686060127832,
+            name: "Text Field",
+            label: "Full Name",
+            value: "",
+            isRequired: true,
+          },
+          {
+            id: 1686060132118,
+            name: "Phone Number",
+            label: "Phone no",
+            value: "",
+            isRequired: true,
+          },
+          {
+            id: 1686060136527,
+            name: "Text Area",
+            label: "Address",
+            value: "",
+            isRequired: true,
+          },
+          {
+            id: 1686060146790,
+            name: "Radio Buttons",
+            label: "Education",
+            value: "",
+            optionsList: ["Btech", "MBA", "BBA", "MCA"],
+            isRequired: true,
+          },
+          {
+            id: 1686060152189,
+            name: "Text Area",
+            label: "Why are you a good fit for the role ?",
+            value: "",
+            isRequired: true,
+          },
+          {
+            id: 1686060162294,
+            name: "Yes / No",
+            label: "Do you have 2+ years of experience ?",
+            value: "",
+            isRequired: true,
+          },
+          {
+            id: 1686060172470,
+            name: "Yes / No",
+            label: "Do you have experience with Svelte ?",
+            value: "",
+            isRequired: true,
+          },
+          {
+            id: 1686060180662,
+            name: "File Upload",
+            label: "Resume",
+            value: "",
+            isRequired: true,
+          },
+        ]; Create form for following scenario and provide me only array in json stringify format without variable. Scenario: ` +
+              " " +
+              prompt,
+          },
+        ],
+      });
+
+      console.log(
+        chat_completion.json().then(async (m) => {
+          if (m?.error) {
+            console.log(m?.error?.message);
+            toast(m?.error?.message, {
+              position: "bottom-right",
+              autoClose: 2000,
+              theme: "light",
+            });
+          } else {
+            let formEls = JSON.parse(m);
+            await createFormFromTemplate(formEls);
+            toast("Created form successfully using provided prompt", {
+              position: "bottom-right",
+              autoClose: 2000,
+              theme: "light",
+            });
+          }
+        })
+      );
+    } catch (err) {
+      toast(err, {
+        position: "bottom-right",
+        autoClose: 2000,
+        theme: "light",
+      });
+    } finally {
+      setIsGeneratingForm(true);
     }
-  }, []);
+  };
+
+  const createFormFromTemplate = async (els) => {
+    try {
+      let user =
+        (typeof window !== undefined &&
+          window.localStorage.getItem("FormVibeUser")) ??
+        "{}";
+      let userInfo = JSON.parse(user);
+      let userId = userInfo?.$id;
+
+      const res = await createForm(userId, "AI Form", els);
+      console.log(res);
+      router.push("/forms/" + res?.form_id);
+    } catch (err) {
+      toast(err);
+    } finally {
+      setIsGeneratingForm(false);
+    }
+  };
+
+  function extractConsultationFormArray(responseContent) {
+    console.log(responseContent);
+    // Find the starting index of the array by locating the first occurrence of '[\n'
+    const startIndex = responseContent.indexOf("[\n");
+    console.log(startIndex);
+
+    // Find the ending index of the array by locating the last occurrence of '\n];'
+    const endIndex = responseContent.lastIndexOf("\n];") + 3;
+    console.log(endIndex);
+
+    // Extract the array portion from the response content
+    const arrayString = responseContent.slice(startIndex, endIndex);
+
+    console.log(arrayString);
+    // Parse the array string into an actual JavaScript array
+    const consultationFormEls = JSON.parse(arrayString);
+
+    return consultationFormEls;
+  }
 
   return (
     <div className="flex flex-col p-6 md:px-12 md:py-8 gap-6 flex-grow">
@@ -62,13 +220,19 @@ function UseAIPage() {
       <div className="flex flex-col gap-2">
         <label>Prompt</label>
         <textarea
-          className="border border-zinc-200 rounded p-2 h-48 resize-none"
+          className="border border-zinc-200 rounded p-2 h-48 resize-none focus:outline-none"
           placeholder="Enter prompt here to describe the scenario for which you need form"
+          value={prompt}
+          onChange={(e) => setPrompt(e.target.value)}
         />
       </div>
-      <span className="btn-primary flex items-center gap-2 w-44 justify-center">
-        Create Form
-      </span>
+      <button
+        onClick={handlePrompts}
+        className="btn-primary flex items-center gap-2 w-44 justify-center"
+        disabled={prompt?.trim().length === 0 || isGeneratingForm}
+      >
+        {isGeneratingForm ? "Creating..." : "Create Form"}
+      </button>
     </div>
   );
 }
